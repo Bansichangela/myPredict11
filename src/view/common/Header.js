526937109a6fa975e5a486cda3b58c4d28013292
LAPTOP-C8ML6OKD\Shree Ganesh <changelabanc20@gmail.com>
2024-04-09 19:23:33 +0530

    Header complete

--- src/view/common/Header.js
+++ src/view/common/Header.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div style={{ backgroundColor: "#6d0202" }}>
       <Container>
@@ -13,17 +20,34 @@ const Header = () => {
             </Link>
           </LogoBox>
           <Menu>
-            <Li href="">Home</Li>
-            <Li href="">About Us</Li>
-            <Li href="">Pages</Li>
-            <Li href="">Blog</Li>
-            <Li href="">Contact</Li>
+            <CustomLink to="/">Home</CustomLink>
+            <CustomLink to="/about">About Us</CustomLink>
+            <CustomLink to="/pages">Pages</CustomLink>
+            <CustomLink to="/blog">Blog</CustomLink>
+            <CustomLink to="/contact">Contact</CustomLink>
             <Button>
               Find Tipster <i class="fa-solid fa-user"></i>
             </Button>
           </Menu>
+          <MobileMenu>
+            <IconButton sx={{ color: "white" }}>
+              <MenuIcon fontSize="large" onClick={handleClick} />
+            </IconButton>
+          </MobileMenu>
         </Navbar>
       </Container>
+      <div style={{ position: "relative" }}>
+        <MobileMenuItem hidden={!isOpen}>
+          <MobileCustomLink to="/">Home</MobileCustomLink>
+          <MobileCustomLink to="/about">About Us</MobileCustomLink>
+          <MobileCustomLink to="/pages">Pages</MobileCustomLink>
+          <MobileCustomLink to="/blog">Blog</MobileCustomLink>
+          <MobileCustomLink to="/contact">Contact</MobileCustomLink>
+          <MenuButton>
+            Find Tipster <i class="fa-solid fa-user"></i>
+          </MenuButton>
+        </MobileMenuItem>
+      </div>
     </div>
   );
 };
@@ -31,11 +55,12 @@ const Header = () => {
 export default Header;
 
 const Container = styled.div`
-  width: 1280px;
+  max-width: 1285px;
   margin: 0 auto;
 `;
 
 const Navbar = styled.div`
+  height: 90px;
   display: flex;
   justify-content: space-between;
   align-items: center;
@@ -52,22 +77,40 @@ const Logo = styled.img`
 
 const Menu = styled.ul`
   display: flex;
+  font-size: 20px;
+  font-family: "Poppins", sans-serif;
   align-items: center;
-  justify-content: space-around;
+  margin-right: 10px;
+  gap: 5px;
+  @media (max-width: 950px) {
+    display: none;
+  }
 `;
 
-const Li = styled.li`
+const MobileMenu = styled.div`
+  display: none;
+  @media (max-width: 950px) {
+    display: block;
+  }
+`;
+
+const CustomLink = styled(Link)`
   color: white;
   margin: 10px;
   display: block;
   text-decoration: none;
+
+  &:hover {
+    color: #f4c566;
+  }
 `;
 
 const Button = styled.button`
   background-color: white;
   color: #6d0202;
   padding: 12px 22px;
-  font-size: 15px;
+  font-size: 20px;
+  font-family: "Poppins", sans-serif;
   font-weight: 800;
   cursor: pointer;
   border-radius: 5px;
@@ -77,7 +120,75 @@ const Button = styled.button`
     color: #6d0202;
     border-radius: 50px;
     border: 2px solid black;
-    background-color: lightgray;
+    background-color: #f4c566;
     transition: all 1s ease 0s;
+  }
+`;
+
+const MenuButton = styled.button`
+  background-color: white;
+  color: #6d0202;
+  padding: 12px 22px;
+  margin-left: 7px;
+  font-size: 15px;
+  font-family: "Poppins", sans-serif;
+  font-weight: 800;
+  width: 45%;
+  cursor: pointer;
+  border-radius: 5px;
+
+  transition: all 1s;
+  &:hover {
+    color: #6d0202;
+    border-radius: 50px;
+    border: 2px solid black;
+    background-color: #f4c566;
+    transition: all 1s ease 0s;
+  }
+`;
+
+const MobileMenuItem = styled.div`
+  position: absolute;
+  // top: 77px;
+  left: 0;
+  right: 0;
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  margin:0 auto;
+  font-size: 2.2rem;
+  font-family: "Poppins", sans-serif;
+  background-color: #f2f2f2;
+  cursor: pointer;
+  transition: height 0.5s ease-in-out;
+  height: ${(props) => (props.hidden ? "0" : "35vh")};
+  overflow-y: scroll;
+  // scrollbar-width: thin; 
+  // scrollbar-color: #888 #f1f1f1;
+
+  &&::-webkit-scrollbar{
+    display: none;
+  }
+  && {
+    -ms-overflow-style: none; / IE and Edge /
+    scrollbar-width: none; / Firefox /
+  }
+
+  @media (min-width: 950px) {
+    display: none;
+  }
+`;
+
+const MobileCustomLink = styled(Link)`
+  display: block;
+  padding: 15px 10px;
+  border-left: 3px solid #f2f2f2;
+  color: #6d0202;
+  text-decoration: none;
+  font-size: 20px;
+  font-weight: 700;
+  &:hover {
+    color: #f4c566;
+    border-color: #f4c566;
   }
 `;
--- src/view/home/Landing.js
+++ src/view/home/Landing.js
@@ -1,7 +1,51 @@
 import React from "react";
+import styled from "styled-components";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Container>
+      <BackgroundImage />
+      <Content>
+        <Title>INVEST BID A LITTLE & GET MORE PROFITABLE PLAY</Title>
+      </Content>
+    </Container>
+  );
 };
 
 export default Landing;
+
+const Container = styled.div`
+  position: relative;
+  height: 675px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const BackgroundImage = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url("./img/bg-3image.jpg");
+  background-size: cover;
+  -webkit-filter: blur(8px);
+  filter: blur(8px);
+`;
+
+const Content = styled.div`
+  z-index: 1;
+  max-width: 924px;
+  margin: 0 auto;
+`;
+
+const Title = styled.h1`
+  color: #6d0202;
+  font-weight: 700;
+  font-size: 50px;
+  display: flex;
+  text-align: center;
+  align-items: center;
+  font-family: "Playfair Display", serif;
+`;
