9d445a6ce66d7b9b97882898049160ae5d725142
LAPTOP-C8ML6OKD\Shree Ganesh <changelabanc20@gmail.com>
2024-04-15 15:04:07 +0530

    Pointpage completed

---
+++ src/view/home/Point.js
@@ -0,0 +1,172 @@
+import React from "react";
+import styled from "styled-components";
+
+const Point = () => {
+  return (
+    <>
+      <Container>
+        <Details>
+          <Title>
+            <div
+              style={{
+                color: "#f7d689",
+                display: "flex",
+                justifyContent: "center",
+                paddingBottom: "15px",
+              }}
+            >
+              Number Said More Than Words
+            </div>
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                color: "white",
+              }}
+            >
+              <h2>The Numbers Don't Lie</h2>
+            </div>
+          </Title>
+
+          <InformationItem>
+            <Average>
+              <Averageimg>
+                <img
+                  src="./img/averageteam.png"
+                  alt="averageteam"
+                  style={{ maxWidth: "100px", height: "auto", padding: "20px" }}
+                ></img>
+              </Averageimg>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  color: "white",
+                  padding: "10px",
+                }}
+              >
+                <h2>176</h2>
+                <h2>Average Team</h2>
+              </div>
+            </Average>
+            <AveragePoint>
+              <AveragePointImg>
+                <img
+                  src="./img/averagepoint.png"
+                  alt="averagepoint"
+                  style={{ maxWidth: "100px", height: "auto", padding: "20px" }}
+                ></img>
+              </AveragePointImg>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  color: "white",
+                  padding: "10px",
+                }}
+              >
+                <h2>785</h2>
+                <h2>Average Point</h2>
+              </div>
+            </AveragePoint>
+            <Predictor>
+              <PredictorImg>
+                <img
+                  src="./img/predictor.png"
+                  alt="predictor"
+                  style={{ maxWidth: "100px", height: "auto", padding: "20px" }}
+                ></img>
+              </PredictorImg>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  color: "white",
+                  padding: "10px",
+                }}
+              >
+                <h2>160</h2>
+                <h2>Predictors</h2>
+              </div>
+            </Predictor>
+          </InformationItem>
+        </Details>
+      </Container>
+    </>
+  );
+};
+
+export default Point;
+
+const Container = styled.div`
+  margin-bottom: 50px;
+  background-color: #6d0202;
+`;
+
+const Details = styled.div`
+  max-width: 85%;
+  margin: 0 auto;
+  gap: 10px;
+`;
+
+const Title = styled.div`
+  margin: 0 auto;
+  padding: 70px 0 30px 0;
+  color: black;
+  font-size: 40px;
+  @media (max-width: 1130px) {
+    font-size: 30px;
+    @media (max-width: 645px) {
+      font-size: 25px;
+      text-align: center;
+    }
+  }
+`;
+
+const Average = styled.div`
+  display: flex;
+`;
+
+const Averageimg = styled.div`
+  width: 100px
+  padding: 20px;
+ 
+`;
+
+const AveragePoint = styled.div`
+  display: flex;
+`;
+
+const AveragePointImg = styled.div`
+  width: 100px
+  padding: 20px;
+  
+`;
+
+const Predictor = styled.div`
+  display: flex;
+`;
+
+const PredictorImg = styled.div`
+  width: 100px
+  padding: 20px;
+  
+`;
+
+const InformationItem = styled.div`
+  display: flex;
+  justify-content: space-between;
+  // flex-direction: column;
+  align-items: center;
+  flex-wrap: wrap;
+  @media (max-width: 1135px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../common/Header";
 import Landing from "./Landing";
 import Footer from "../common/Footer";
+import Point from "./Point";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Landing />
+      <Point />
       <Footer />
     </>
   );
